1c504921b8c9fbdd41327008418b16bcd5c870a8
JATIN-J3 <138765067+JATIN-J3@users.noreply.github.com>
2024-11-10 14:31:39 +0530

    Update AppConfig.java

--- src/main/java/com/zosh/config/AppConfig.java
+++ src/main/java/com/zosh/config/AppConfig.java
@@ -49,7 +49,7 @@ public class AppConfig {
 				cfg.setAllowedOrigins(Arrays.asList(
 						"http://localhost:3000",
 						"http://localhost:4000",
-						"http://localhost:4200",
+						"http://localhost:4200"
 
 						
 
